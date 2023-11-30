53229b7dd0843de6041b5f03a6c244d2a1def230
nguyenvanhau896 <nguyenvanhau896@gmail.com>
2023-11-30 10:27:20 +0700

    correct url in product

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -17,7 +17,7 @@
                 </a>
             </div>
             <div class="col-span-1 bg-[#55dca2] rounded-3xl transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-[#55dc6a] duration-300">
-                <a href="/techshop_admin/admin/product/index" class="p-4 flex flex-col items-center align-center">
+                <a href="/techadmin/admin/product/index" class="p-4 flex flex-col items-center align-center">
                     <img src="https://static.thenounproject.com/png/1375595-200.png" alt="icon" class="w-1/3 mb-2">
                     <p class="text-lg font-semibold">Sản phẩm đang bán</p>
                 </a>
